172a1fe30e452cd8f272a4e144722670b83a0f25
Marcos Cordeiro <marcosc974@gmail.com>
2022-05-19 16:52:20 -0300

    Atualização das rotas

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ $message = [];
 $app = new \Slim\App();
 
 #Middleware para autenticação
-$mid01 = function($request, $response, $next){
+$mid01 = function ($request, $response, $next) {
     // $_SESSION['logado'] = true;
-    if(isset($_SESSION['logado'])){
-        $response = $next($request,$response);
+    if (isset($_SESSION['logado'])) {
+        $response = $next($request, $response);
         return $response;
-    }else{
+    } else {
         return $response->withRedirect('login');
     }
 };
@@ -29,20 +29,20 @@ $app->get('/', HomeController::class . ':home')->setName('home');
 
 $app->post('/', function ($request, $response, $args) {
     $data = $request->getParsedBody();
-    if(HomeController::insertGarantia($data)){
+    if (HomeController::insertGarantia($data)) {
         echo HomeController::insertGarantia($data);
-    }else{
+    } else {
         echo HomeController::insertGarantia($data);
     }
 });
 
 $app->get('/login', LoginController::class . ':login');
 
-$app->post('/login',function($request, $response, $args){
+$app->post('/login', function ($request, $response, $args) {
     $data = $request->getParsedBody();
-    if(AdminController::login($data)){
+    if (AdminController::login($data)) {
         return $response->withRedirect('admin');
-    }else{
+    } else {
         echo "Faça login primeiro";
     }
 });
@@ -61,14 +61,15 @@ $app->get('/admin', AdminController::class . ':viewAdminDashboard')->add($mid01)
 
 $app->get('/listar', AdminController::class . ':getRegistros')->add($mid01);
 
-$app->post('/detalhe', function($request, $response, $args){
+$app->post('/detalhe', function ($request, $response, $args) {
+
     $id = $request->getParsedBody();
     AdminController::detail($id['usu_id']);
 })->add($mid01);
 
 $app->get('/profile', AdminController::class . ':mydata')->add($mid01);
 
-$app->post('/profile', function($request, $response, $args){
+$app->post('/profile', function ($request, $response, $args) {
     $data = $request->getParsedBody();
     AdminController::updateData($data);
     return $response->withRedirect('profile');
@@ -76,12 +77,12 @@ $app->post('/profile', function($request, $response, $args){
 
 $app->get('/novo', AdminController::class . ':viewAddUser')->add($mid01);
 
-$app->post('/novo', function($request, $response, $args){
+$app->post('/novo', function ($request, $response, $args) {
     $data = $request->getParsedBody();
     AdminController::insertUsuario($data);
 })->add($mid01);
 
-$app->get('/logout',function($request, $response, $args){
+$app->get('/logout', function ($request, $response, $args) {
     session_start();
     session_destroy();
     return $response->withRedirect('/');
