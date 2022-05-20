597c8c54124e1d8b414873f28d37a50ca0ff1f4e
Marcos Cordeiro <marcosc974@gmail.com>
2022-05-20 16:31:37 -0300

    AJUSTES ENV

--- env-model.php
+++ env-model.php
@@ -1,9 +1,18 @@
 <?php
-//------------------ LOCALHOST-------------
-//Para que o arquivo funcione : renomeie ele de env-model.php para env.php
-//Altere os campos abaixo informando as suas configurações de acesso
+#-------------------------------------------#
+# CONFIGURAÇÕES DO BANCO DE DADOS PDO       #
+#-------------------------------------------#
+
+#------------------- LOCALHOST-------------
+
 putenv("HOST=YOUR_HOST");
 putenv("DB=YOUR_DB_NAME");
 putenv("USER=YOUR_USER_NAME");
 putenv("PASSWORD=YOUR_PASSWORD");
 
+#------------------ GO LIVE -----------------#
+
+// putenv("HOST=YOUR_HOST");
+// putenv("DB=YOUR_DB_NAME");
+// putenv("USER=YOUR_USER_NAME");
+// putenv("PASSWORD=YOUR_PASSWORD");
